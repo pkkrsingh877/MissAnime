8ae194bddc1ba7d910ae08cadbd0011b58cfad78
Prabhat Kumar <pkkrsingh877@gmail.com>
2022-01-01 11:50:08 +0530

    working on delete feature

--- routes/admin.js
+++ routes/admin.js
@@ -2,6 +2,17 @@ const express = require('express');
 const router = express.Router();
 const News = require('../models/news');
 
+router.delete('/articles/delete/:id', async (req, res) => {
+    try {
+        const { id } = req.body;
+        await News.findByIdAndDelete(id);
+        res.redirect('/admin');
+    } catch (err) {
+        console.log(err);
+        res.redirect('/admin');
+    }
+});
+
 router.get('/', async (req, res) => {
     try {
         const news = await News.find({});
