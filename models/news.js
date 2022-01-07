86c1e9f26ef52a724de1181565605bfe036c6833
Prabhat Kumar <pkkrsingh877@gmail.com>
2022-01-07 10:39:48 +0530

    added slug to model

--- models/news.js
+++ models/news.js
@@ -9,6 +9,11 @@ const newsSchema = new mongoose.Schema({
     description: {
         type: String,
         required: true
+    },
+    slug: {
+        type: String,
+        required: true,
+        unique: true
     }
 });
 
